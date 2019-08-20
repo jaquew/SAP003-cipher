bdf058f883b2bb8bd53296bc745478c8fe98150b
jaquew <eng.jaquesilva.com>
2019-08-20 17:59:42 -0300

    Funcoes encode e decode

--- src/cipher.js
+++ src/cipher.js
@@ -3,22 +3,87 @@ window.cipher = {
   decode: decode
 };
 
-function encode(){
-  let text = "queijo";
-  let codificatxt = new Array;
-  
-  for (let i in text){
-    codificatxt.push(text.charCodeAt(i))
-    i++;
+let text;
+let offset;
+
+function encode(text,offset){
+  //desconsidera espaços, converte maiusculas para minusculas
+  //melhorar o codigo, muito extenso.
+  text = text.toUpperCase();
+
+  let num_array = new Array;
+  let text_array = new Array;
+
+  console.log(text)
+
+  for (let i in text) {
+    
+    //se for espaço, ponto ou virgula  desconsiderar
+    if (text.charAt(i)===" " || text.charAt(i)==="." ||text.charAt(i)===","){
+      text_array[i]=text.charAt(i);
+    
+    } else {
+      //tranformar de letra para numero e somar o deslocamento
+      num_array[i]=(text.charCodeAt(i)-65 + offset)
+
+      //se o resultado da soma for maior que 25(z), começa de 0(a)
+      if (num_array[i]>25){
+        num_array[i]=num_array[i]-26
+      }
+
+      // se estiver fora do alfabeto (0 a 25)
+      if (num_array[i]>25 || num_array[i]<0){
+        text_array[i]=text.charAt(i)
+      } else {
+        //tranformar de numero para letra
+        text_array.push(String.fromCharCode(num_array[i]+65))
+      }
+    }
   }
 
-  console.log(codificatxt)
+  console.log(num_array)
+  console.log(text_array)
 
-  
+  //concatenar o array resultado
+  return text_array.join('')  
 
 }
 
-function decode(){
-  alert("decode");
+
+
+
+
+function decode(text,offset){
+  text = text.toUpperCase();
+
+  let num_array = new Array;
+  let text_array = new Array;
+  
+
+  for (i in text) {
+    
+    if (text.charAt(i)===" " || text.charAt(i)==="." ||text.charAt(i)===","){
+      text_array[i]=text.charAt(i);
+    
+    } else {
+      num_array[i]=(text.charCodeAt(i)-65 - offset)
+
+      if (num_array[i]<0){
+        num_array[i]=num_array[i]+26
+      }
+
+      if (num_array[i]>25 || num_array[i]<0){
+        text_array[i]=text.charAt(i)
+      } else {
+        text_array.push(String.fromCharCode(num_array[i]+65))
+      }
+  
+    }
+  }
+
+  console.log(num_array)
+  console.log(text_array)
+
+  return text_array.join('') 
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 function acao1(){
-    window.cipher.encode()
+    let text = document.getElementById("decoded").value
+    let offset = Number(document.getElementById("offset").value)
+    let encodedtxt = document.getElementById("encoded")
+
+    encodedtxt.innerHTML=(window.cipher.encode(text,offset))
+
+    // document.getElementById("encoded").value=encodedtxt
+
 }
 
 function acao2(){
-    window.cipher.decode()
+    let text = document.getElementById("encoded").value
+    let offset = Number(document.getElementById("offset").value)
+    let decodedtxt = document.getElementById("decoded")
+
+    decodedtxt.innerHTML=(window.cipher.decode(text,offset))
 }
