edf3a207bb4cb2cec13cee66ef76cd40bfaf98f6
jaquew <eng.jaquesilva.com>
2019-08-21 15:56:00 -0300

    considerar offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -6,30 +6,27 @@ window.cipher = {
 function encode(text,offset){
   //desconsidera espaços, converte maiusculas para minusculas
   //melhorar o codigo, muito extenso.
+  
 
   let num_array = new Array;
   let text_array = new Array;
 
-  console.log(text)
-
   for (let i in text) {
+
     // se estiver fora do alfabeto
     if (text.charCodeAt(i)<65 || text.charCodeAt(i)>90){
     text_array[i]=text.charAt(i)    
     } else {
       //tranformar de letra para numero e somar o deslocamento
-      num_array[i]=(text.charCodeAt(i)-65 + offset)%26 +65
+      num_array[i]=(text.charCodeAt(i)+65 + offset)%26 +65
       //tranformar de numero para letra
       text_array.push(String.fromCharCode(num_array[i]))
     }
   }
-
-  console.log(num_array)
-  console.log(text_array)
+  document.ge
 
   //concatenar o array resultado
   return text_array.join('')  
-
 }
 
 
@@ -39,22 +36,15 @@ function decode(text,offset){
   let num_array = new Array;
   let text_array = new Array;
   
-
   for (let i in text) {
-    // se estiver fora do alfabeto
+
     if (text.charCodeAt(i)<65 || text.charCodeAt(i)>90){
     text_array[i]=text.charAt(i)    
     } else {
-      //tranformar de letra para numero e subtrair o deslocamento
-      num_array[i]=(text.charCodeAt(i)-65 - offset) % 26 + 65
-      //tranformar de numero para letra
+      num_array[i]=(text.charCodeAt(i)+ 65 - offset) % 26 + 65
       text_array.push(String.fromCharCode(num_array[i]))
     }
   }
 
-  console.log(num_array)
-  console.log(text_array)
-
   return text_array.join('') 
-
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-function acao1(){
-    // event.preventDefault()
+function encodetxt(){
     let text = document.getElementById("decoded").value
     text = text.toUpperCase();
+    
     let offset = Number(document.getElementById("offset").value)
-    let encodedtxt = document.getElementById("encoded")
+    //let encodedtxt = document.getElementById("encoded")
 
-    encodedtxt.innerHTML=(window.cipher.encode(text,offset))
-
-    // document.getElementById("encoded").value=encodedtxt
+    // encodedtxt.innerHTML=(window.cipher.encode(text,offset))
+    if (text===""){
+        alert("Por favor, digite uma mensagem.")
+    } else {
+        document.getElementById("encoded").value=window.cipher.encode(text,offset)
+    }
 
 }
 
-function acao2(){
-    // event.preventDefault()
+function decodetxt(){
     let text = document.getElementById("encoded").value
     text = text.toUpperCase();
     let offset = Number(document.getElementById("offset").value)
-    let decodedtxt = document.getElementById("decoded")
+    //let decodedtxt = document.getElementById("decoded")
 
-    decodedtxt.innerHTML=(window.cipher.decode(text,offset))
+    //decodedtxt.innerHTML=(window.cipher.decode(text,offset))
+    if (text===""){
+        alert("Por favor, digite uma mensagem.")
+    } else {
+    document.getElementById("decoded").value=window.cipher.decode(text,offset)
+    }
 }
