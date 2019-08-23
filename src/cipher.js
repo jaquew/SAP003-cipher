3cd68e553ace37de421d99ca2a3595fd61375eb7
jaquew <eng.jaquesilva.com>
2019-08-23 17:35:12 -0300

    correcao de erros, adequacao aos testes

--- src/cipher.js
+++ src/cipher.js
@@ -3,52 +3,50 @@ window.cipher = {
   decode: decode
 };
 
-function encode(text,offset){
+function encode(offset, text) {
 
-  let num_array = new Array;
-  let text_array = new Array;
+  let numArray = new Array;
+  let textArray = new Array;
 
   for (let i in text) {
 
     if (97<=text.charCodeAt(i) && text.charCodeAt(i)<=122) {//se for minuscula
       //tranformar de letra para numero e somar o deslocamento
-      num_array[i]=(text.charCodeAt(i)+33 + offset)%26 +97
+      numArray[i]=(text.charCodeAt(i)+33 + offset)%26 +97;
       //tranformar de numero para letra
-      text_array[i]=(String.fromCharCode(num_array[i]))
+      textArray[i]=(String.fromCharCode(numArray[i]));
         
     } else if (65<=text.charCodeAt(i) && text.charCodeAt(i)<=90) {//se for maiuscula
-      num_array[i]=(text.charCodeAt(i)+39 + offset)%26 +65
-      text_array[i]=(String.fromCharCode(num_array[i]))
+      numArray[i]=(text.charCodeAt(i)+39 + offset)%26 +65;
+      textArray[i]=(String.fromCharCode(numArray[i]));
 
-    }else {
+    } else {
       // se estiver fora do alfabeto
-      text_array[i]=text.charAt(i)
+      textArray[i]=text.charAt(i);
     }
   }
- console.log(num_array)
 
   //concatenar o array resultado
-  return text_array.join('')  
+  return textArray.join("") ; 
 }
 
-
-function decode(text,offset){
-  let num_array = new Array;
-  let text_array = new Array;
+function decode(offset, text) {
+  let numArray = new Array;
+  let textArray = new Array;
   
   for (let i in text) { 
     if (97<=text.charCodeAt(i) && text.charCodeAt(i)<=122) {
-      num_array[i]=(text.charCodeAt(i)+33 - offset)%26 +97
-      text_array[i]=(String.fromCharCode(num_array[i]))
+      numArray[i]=(text.charCodeAt(i)+33 - offset)%26 +97;
+      textArray[i]=(String.fromCharCode(numArray[i]));
         
     } else if (65<=text.charCodeAt(i) && text.charCodeAt(i)<=90) {
-      num_array[i]=(text.charCodeAt(i)+39 - offset)%26 +65
-      text_array[i]=(String.fromCharCode(num_array[i]))
+      numArray[i]=(text.charCodeAt(i)+39 - offset)%26 +65;
+      textArray[i]=(String.fromCharCode(numArray[i]));
 
-    }else {
-      text_array[i]=text.charAt(i)
+    } else {
+      textArray[i]=text.charAt(i);
     }
   }
 
-  return text_array.join('') 
+  return textArray.join("");
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-function encodetxt(){
-    let text = document.getElementById("decoded").value
-    let offset = Number(document.getElementById("offset").value)
+document.getElementById("cifrar").addEventListener("click", function encodetxt() {
+  let text = document.getElementById("decoded").value;
+  let offset = Number(document.getElementById("offset").value);
 
-    if (text===""){
-        alert("Por favor, digite uma mensagem.")
-    } else {
-        document.getElementById("encoded").value=window.cipher.encode(text,offset)
-    }
+  if (text==="") {
+    alert("Por favor, digite uma mensagem.");
+  } else {
+    document.getElementById("encoded").value=cipher.encode(offset, text);
+  }
 }
+);
 
-function decodetxt(){
-    let text = document.getElementById("encoded").value
-    let offset = Number(document.getElementById("offset").value)
+document.getElementById("decifrar").addEventListener("click", function decodetxt() {
+  let text = document.getElementById("encoded").value;
+  let offset = Number(document.getElementById("offset").value);
 
-    if (text===""){
-        alert("Por favor, digite uma mensagem.")
-    } else {
-    document.getElementById("decoded").value=window.cipher.decode(text,offset)
-    }
+  if (text==="") {
+    alert("Por favor, digite uma mensagem.");
+  } else {
+    document.getElementById("decoded").value=window.cipher.decode(offset, text);
+  }
 }
+);
 
-function cleartxt(){
-    document.getElementById("encoded").value=""
-    document.getElementById("decoded").value=""
-    document.getElementById("offset").value="1"
+document.getElementById("clear").addEventListener("click", function cleartxt() {
+  document.getElementById("encoded").value="";
+  document.getElementById("decoded").value="";
+  document.getElementById("offset").value="1";
 }
+);
